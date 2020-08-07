579ac48d56da7b6a3316ac1b7d3e635907b25423
Johannes Nicolaus <johannes.nicolaus@gmail.com>
2020-08-07 23:39:01 +0900

    first commit

---
+++ convert_id/server.R
@@ -0,0 +1,70 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(biomaRt)
+library(DT)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+
+    
+    observe({
+        
+        organism <- input$organism
+        
+        all_attributes <- listAttributes(useMart("ensembl", dataset = paste0(organism,"_gene_ensembl"), host = input$host))
+        
+        updateSelectizeInput(session,
+                             "convert_to",
+                             label = "Convert to:",
+                             choices = all_attributes$name)
+    }) 
+    
+    
+    
+    
+    
+    
+
+# output table ------------------------------------------------------------
+    
+    output$converted_table <- DT::renderDataTable({
+        
+        inFile <- input$file1
+        
+        if (is.null(inFile))
+            return(NULL)
+        
+        # read input file
+        input_genes <- read_csv(inFile$datapath) %>% pull(1)
+        
+        organism <- input$organism
+        
+        converted_genes <- getBM(attributes= c(input$keytype, input$convert_to),
+                                 filters = input$keytype,
+                                 values = input_genes,
+                                 mart = useMart("ensembl", dataset = paste0(organism,"_gene_ensembl"), host = input$host)
+                                 # uniqueRows = unique_rows
+        )
+        
+        print(converted_genes)
+        
+    },
+    extensions = c('Buttons'), 
+    options = list(
+        dom = 'Bfrtip',
+        buttons = c('copy', 'csv', 'excel', 'pdf', 'print')
+    ),
+   # align = "c",
+   # width = "100%"
+    )
+
+})
---
+++ convert_id/ui.R
@@ -0,0 +1,67 @@
+
+library(shiny)
+
+file_input <- fileInput("file1", "Choose csv File",
+                        accept = c(
+                            "text/csv",
+                            "text/comma-separated-values,text/plain",
+                            ".csv")
+)
+
+
+organism_input <- selectInput("organism", "Organism:",
+                              c("Human" = "hsapiens",
+                                "Mouse" = "mmusculus",
+                                "Chicken" = "ggallus"))
+
+keytype_input <- selectInput("keytype", "Type of ID supplied:",
+                              c("Ensembl gene id" = "ensembl_gene_id",
+                                "Ensembl transcript id" = "ensembl_transcript_id",
+                                "External gene id" = "external_gene_id"))
+
+host_input <- selectInput("host", "Ensembl host (only use when default doesn't work):",
+                             c("Asia" = "asia.ensembl.org",
+                               "West US" = "uswest.ensembl.org",
+                               "East US" = "useast.ensembl.org"))
+
+convert_input <- selectizeInput(
+    "convert_to", "Convert to (might take a while to load, be patient!):" , choices = NULL,
+    options = list(create = TRUE), multiple = T
+)
+
+
+# output table
+
+table_output <- div(style="height:600px; overflow:scroll; align:text-center",
+    DT::dataTableOutput("converted_table")
+)
+
+
+# download_output <- downloadButton("downloadData", "Download")
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Convert gene ID"),
+    h3("By Johannes Nicolaus Wibisana 2020/08/07"),
+    h4("Accepts csv with header, first column will be read as input"),
+    
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            file_input,
+            organism_input,
+            keytype_input,
+            host_input,
+            convert_input
+            
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            table_output
+        )
+    )
+))
